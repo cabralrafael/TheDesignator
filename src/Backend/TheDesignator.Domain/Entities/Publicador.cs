0b867ce92bd8b749eea17d3d060a5e3c255631d6
cabralrafael <k.rafael@hotmail.com>
2026-07-29 16:36:47 -0300

    feature: Criação da classe que define um publicador

---
+++ src/Backend/TheDesignator.Domain/Entities/Publicador.cs
@@ -0,0 +1,10 @@
+﻿namespace TheDesignator.Domain.Entities;
+
+public class Publicador
+{
+    public string Nome { get; set; } = string.Empty;
+
+    public string Sexo { get; set; } = string.Empty;
+
+    public List<Privilegio>? Privilegios { get; set; }
+}
